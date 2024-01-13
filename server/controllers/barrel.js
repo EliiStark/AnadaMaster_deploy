b5bdc3f9934e2e7786402bb36f6729107f4bf583
Elisa Alonso <elisaalonsosa@gmail.com>
2024-01-13 20:30:22 +0100

    Update barrel.js

--- server/controllers/barrel.js
+++ server/controllers/barrel.js
@@ -8,6 +8,7 @@ const { handleHttpError } = require('../utils/handleHttpError.js')
  * @param {*} res 
  */
 
+//hola
 const getBarrels = async (req, res) => {
     try{
         const data = await barrelModel.find().lean() //las llaves {} vacías significa que me coja todo
